fe472c5362079cfc809d11056f52554cfeb1ec05
joedevpk <joedevpk@gmail.com>
2026-04-08 18:38:10 +0200

    Update context_processors.py

--- elearning/context_processors.py
+++ elearning/context_processors.py
@@ -2,5 +2,5 @@ def global_context(request):
     return {
         'qr_site': request.build_absolute_uri('/'),
         'qr_whatsapp': 'https://wa.me/243816172056',
-        'qr_tiktok': 'https://www.tiktok.com/@tonprofil'
+        'qr_tiktok': 'https://www.tiktok.com/@joe.dev.codes?_r=1&_t=ZS-95N7FXYeZZB'
     }
