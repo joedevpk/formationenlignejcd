18217868ab8cc98f3947c3c18a413d67f422a9f6
joedevpk <joedevpk@gmail.com>
2026-05-04 01:44:16 +0200

    Update asgi.py

--- elearning/asgi.py
+++ elearning/asgi.py
@@ -3,19 +3,26 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-import notifications.routing
-from chat.routing import websocket_urlpatterns as chat_ws
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearning.settings')
 
 django_asgi_app = get_asgi_application()
 
+# IMPORT SAFE (évite crash Render)
+try:
+    import chat.routing
+    chat_ws = chat.routing.websocket_urlpatterns
+except Exception:
+    chat_ws = []
+
+try:
+    import notifications.routing
+    notif_ws = notifications.routing.websocket_urlpatterns
+except Exception:
+    notif_ws = []
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat_ws + notifications.routing.websocket_urlpatterns
-        )
+        URLRouter(chat_ws + notif_ws)
     ),
 })
