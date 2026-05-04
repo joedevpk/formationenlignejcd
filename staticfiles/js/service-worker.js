e2d27d7737bb44ba986e4114a7836d1f7513a13b
Joe Dev <tonemail@gmail.com>
2026-05-04 22:31:40 +0200

    resolve merge conflict SW

--- staticfiles/js/service-worker.js
+++ staticfiles/js/service-worker.js
@@ -1,40 +1,33 @@
-const CACHE_NAME = "jdc-academy-v1";
+const CACHE_NAME = "jdc-academy-v2";
 
 const urlsToCache = [
   "/",
-  "/static/css/style.css",
-  "/static/js/main.js"
+  "/static/css/style.css"
 ];
 
 // INSTALL
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 // ACTIVATE
 self.addEventListener("activate", event => {
+  self.clients.claim();
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
   );
 });
 
-// FETCH
+// FETCH (offline fallback)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(res => res || caches.match("/"))
+    )
   );
 });
