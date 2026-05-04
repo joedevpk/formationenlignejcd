2f8a6e9be312cd8f97e72a55a68cf06c49d1a2b8
joedevpk <joedevpk@gmail.com>
2026-05-04 12:27:42 +0200

    Update signals.py

--- chat/signals.py
+++ chat/signals.py
@@ -1,15 +1,25 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
+from django.db.utils import OperationalError, ProgrammingError
 from .models import Profile
+
 
 @receiver(user_logged_in)
 def online(sender, request, user, **kwargs):
-    profile, _ = Profile.objects.get_or_create(user=user)
-    profile.is_online = True
-    profile.save()
+    try:
+        profile, _ = Profile.objects.get_or_create(user=user)
+        profile.is_online = True
+        profile.save()
+    except (OperationalError, ProgrammingError):
+        # évite crash serveur si table pas prête
+        pass
+
 
 @receiver(user_logged_out)
 def offline(sender, request, user, **kwargs):
-    profile, _ = Profile.objects.get_or_create(user=user)
-    profile.is_online = False
-    profile.save()
+    try:
+        profile, _ = Profile.objects.get_or_create(user=user)
+        profile.is_online = False
+        profile.save()
+    except (OperationalError, ProgrammingError):
+        pass
