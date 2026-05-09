d7aa13f347590178a8e870269f67ce6b2d745da5
Joe Dev <tonemail@gmail.com>
2026-05-09 22:34:56 +0200

    setup redis channels

--- elearning/settings.py
+++ elearning/settings.py
@@ -79,19 +79,13 @@ ASGI_APPLICATION = 'elearning.asgi.application'
 
 
 # CHANNELS (notifications)
-"""
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.getenv("REDIS_URL")],
+            "hosts": [os.environ.get("REDIS_URL")],
         },
-    },
-}
-"""
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
 
