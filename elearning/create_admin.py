d38279a5b65a6f9cfc1557e3c82f767602e4e7dd
joedevpk <joedevpk@gmail.com>
2026-07-08 21:38:10 +0200

    Update create_admin.py

--- elearning/create_admin.py
+++ elearning/create_admin.py
@@ -2,24 +2,22 @@ import os
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elearning.settings")
-
 django.setup()
 
-from accounts.models import User
+from django.contrib.auth import get_user_model
 
-username = os.environ.get("ADMIN_USERNAME")
-email = os.environ.get("ADMIN_EMAIL")
-password = os.environ.get("ADMIN_PASSWORD")
+User = get_user_model()
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password
-        )
-        print("Superuser créé")
-    else:
-        print("Superuser existe déjà")
+username = "Joepiako"
+email = "joepiako@gmail.com"
+password = "piako2005"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+    print("Superuser créé.")
 else:
-    print("Variables admin manquantes")
+    print("Le superuser existe déjà.")
