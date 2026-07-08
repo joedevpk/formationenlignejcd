50688ad27a3f171f9a65fafbd0426c3e0e912ce4
joedevpk <joedevpk@gmail.com>
2026-07-08 22:26:06 +0200

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -1,23 +1,22 @@
 import os
 import django
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "elearning.settings"
-)
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ton_projet.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
+username = "Joepiako"
+
+if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(
-        username="Joepiako",
-        email="piako@gmail.com",
+        username=username,
+        email="joepiako@gmail.com",
         password="piako2005"
     )
     print("Superuser créé")
 else:
-    print("Superuser existe déjà")
+    print("Le superuser existe déjà")
+
